ad3f22b865df7a68c8eb97314622dd2a3248e9cc
asdtiang <asdtiangxia@163.com>
2015-07-23 11:27:15 +0800

    junjie-form-war project create

--- junjie-form-war/src/test/groovy/com/agilemaster/asdtiang/study/Test.groovy
+++
@@ -1,8 +0,0 @@
-package com.agilemaster.asdtiang.study
-
-import com.agilemaster.findoil.bootstrap.CarBrandInitService
-import com.alibaba.fastjson.JSON
-import com.alibaba.fastjson.JSONArray
-import com.alibaba.fastjson.JSONObject
-
-
